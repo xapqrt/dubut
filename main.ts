6b79eeef8e7787205d67f10e84d09efa33ac3c2f
vivaan <xapqrt@vivaans-MacBook-Air-2.local>
2026-05-29 22:08:32 +0530

    added status bar and ribbon icon, basic UI layout registered

--- main.ts
+++ main.ts
@@ -17,6 +17,16 @@ export default class DebatePartnerPlugin extends Plugin {
 		console.log("loading debate partner... M4 is warming up");
 		
 		await this.loadSettings();
+
+		// Create ribbon icon
+		const ribbonIconEl = this.addRibbonIcon('swords', 'Debate Partner', (evt: MouseEvent) => {
+			console.log("ribbon clicked, but need editor selection for thesis_junk");
+		});
+		ribbonIconEl.addClass('debate-partner-ribbon-class');
+
+		// Status bar shows status
+		const statusBarItemEl = this.addStatusBarItem();
+		statusBarItemEl.setText('Debate Partner: Ready');
 		
 		// obsidian's cache api is literally a maze, need to remember to hook it up
 	}
